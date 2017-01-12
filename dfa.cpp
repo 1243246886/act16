eec02535980df04d7d1f890d9a6e816e15e41f28
Iru Cai <mytbk920423@gmail.com>
2017-01-12 21:13:36 +0800

    gen set calculation, bug fixes

--- dfa.cpp
+++ dfa.cpp
@@ -1,4 +1,5 @@
 #include "dfa.hpp"
+#include "flowgraph.hpp"
 #include "ast-nodes.hpp"
 
 #define VALID_VAR(value, fun) ((value<0 && -value<=fun->localspace) \
@@ -7,16 +8,17 @@
 void buildvmap(Function *f, DataFlowInfo &info)
 {
 	int nv = 0;
+	map<string,int> &vmap = info.vmap;
 	for (size_t i=0; i<f->ilist.size(); i++) {
 		Instruction *ins = f->ilist[i];
 		for (size_t j=0; j<2; j++) {
-			Operand *op = ins->operands[i];
+			Operand *op = ins->operands[j];
 			if (op!=NULL) {
-				if (op->operand_type==OPERAND_OFFSET) {
-					if (VALID_VAR(op->v, f)) {
-						info.vmap[op->name] = nv;
-						nv++;
-					}
+				if (op->operand_type==OPERAND_OFFSET
+				    && VALID_VAR(op->v, f)
+				    && vmap.find(op->name)==vmap.end()) {
+					info.vmap[op->name] = nv;
+					nv++;
 				}
 			}
 		}
@@ -56,6 +58,29 @@ void find_killset(Function *f, DataFlowInfo &info)
 			if (j==i) continue;
 			if (info.defines[j]==info.defines[i]) {
 				info.kill[i].insert(j);
+			}
+		}
+	}
+}
+
+void bbGenSet(FlowGraph &g, DataFlowInfo &info)
+{
+	size_t len = g.bb.size();
+	info.gen.resize(len);
+	for (size_t i=0; i<len; i++) {
+		info.gen[i] = set<int>();
+	}
+	for (size_t i=0; i<len; i++) {
+		int end = (i<len-1)?g.bb[i+1]:g.f->ilist.size();
+		set<int> gs;
+		gs.clear();
+		for (int j=end-1; j>=g.bb[i]; j--) {
+			int defv = info.defines[j];
+			if (defv>=0) {
+				if (gs.find(defv)==gs.end()) {
+					info.gen[i].insert(j);
+					gs.insert(defv);
+				}
 			}
 		}
 	}
--- dfa.hpp
+++ dfa.hpp
@@ -5,15 +5,29 @@
 #include <vector>
 #include <map>
 #include <set>
+#include "ast-nodes.hpp"
+#include "flowgraph.hpp"
 
 using namespace std;
 
 struct DataFlowInfo
 {
+	/* variable id to integer mapping */
 	map<string,int> vmap;
+	/* the variable each instruction defines */
 	vector<int> defines;
+	/* kill set of each instruction */
 	vector<set<int> > kill;
+	/* gen set for a basic block,
+	   store the instruction index
+	   of the function
+	*/
 	vector<set<int> > gen;
 };
+
+void buildvmap(Function *f, DataFlowInfo &info);
+void ins_defines(Function *f, DataFlowInfo &info);
+void find_killset(Function *f, DataFlowInfo &info);
+void bbGenSet(FlowGraph &g, DataFlowInfo &info);
 
 #endif
